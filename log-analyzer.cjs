a3b3d8e844cfd6e0f372476aa261db455c2d29ee
goudan <goudan@automaton.tech>
2026-03-25 13:58:41 +0800

    chore: goudan 自动更新

--- log-analyzer.cjs
+++ log-analyzer.cjs
@@ -1,28 +1,53 @@
-const { read_file, write_file, exec_command, list_directory } = require('./tools');
-
-// 日志解析器
-const parseLog = (logContent) => {
-  // TODO: 实现日志解析逻辑
-};
-
-// 错误统计器
-const countErrors = (parsedLogs) => {
-  // TODO: 实现错误统计逻辑
-};
-
-// 搜索引擎
-const searchLogs = (parsedLogs, keyword, timeRange, logLevel) => {
-  // TODO: 实现搜索和过滤逻辑
-};
-
-// 报告生成器
-const generateReport = (parsedLogs, exportFormat) => {
-  // TODO: 实现导出功能
-};
+const { read_file, write_file, exec_command, list_directory } = require('./utils');
 
 module.exports = {
-  parseLog,
-  countErrors,
-  searchLogs,
-  generateReport
+  parseLog: async (filePath) => {
+    const content = await read_file(filePath);
+    return content;
+  },
+
+  countErrors: async (logContent) => {
+    const errorTypes = logContent.match(/Error:.*/g);
+    const errorCounts = errorTypes.reduce((acc, errorType) => {
+      const type = errorType.match(/Error:(.*)/)[1];
+      acc[type] = (acc[type] || 0) + 1;
+      return acc;
+    }, {});
+
+    const sortedErrorCounts = Object.entries(errorCounts).sort((a, b) => b[1] - a[1]);
+    const mostFrequentError = sortedErrorCounts[0][0];
+
+    return {
+      errorCounts,
+      mostFrequentError
+    };
+  },
+
+  searchLogs: async (logContent, keyword, startTime, endTime, level) => {
+    const regex = new RegExp(keyword, 'i');
+    let filteredLogs = logContent.match(regex);
+
+    if (startTime) {
+      filteredLogs = filteredLogs.filter(log => log.includes(startTime));
+    }
+
+    if (endTime) {
+      filteredLogs = filteredLogs.filter(log => log.includes(endTime));
+    }
+
+    if (level) {
+      filteredLogs = filteredLogs.filter(log => log.includes(level));
+    }
+
+    return filteredLogs;
+  },
+
+  exportLogs: async (logContent, format) => {
+    if (format === 'json') {
+      return JSON.stringify(logContent);
+    } else if (format === 'csv') {
+      return logContent.join('
+');
+    }
+  }
 };
--- path/to/file2.js
+++ path/to/file2.js
@@ -1,35 +1,3 @@
-// file2.js
-// 用户界面逻辑
-
-function displayResult(operation, a, b, result) {
-    console.log(`${operation} ${a} ${b} = ${result}`);
+const add = (a, b) => {
+    return a + b;
 }
-
-function handleUserInput() {
-    const operation = prompt('Enter operation (add, subtract, multiply, divide):');
-    const a = parseFloat(prompt('Enter first number:'));
-    const b = parseFloat(prompt('Enter second number:'));
-
-    let result;
-    switch (operation) {
-        case 'add':
-            result = require('./file1').add(a, b);
-            break;
-        case 'subtract':
-            result = require('./file1').subtract(a, b);
-            break;
-        case 'multiply':
-            result = require('./file1').multiply(a, b);
-            break;
-        case 'divide':
-            result = require('./file1').divide(a, b);
-            break;
-        default:
-            console.log('Invalid operation');
-            return;
-    }
-
-    displayResult(operation, a, b, result);
-}
-
-handleUserInput();
