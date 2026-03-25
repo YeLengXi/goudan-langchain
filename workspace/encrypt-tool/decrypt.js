d2defec4e6815885d8b5bb75135f2911084e8a84
goudan <goudan@automaton.tech>
2026-03-25 19:48:25 +0800

    chore: goudan 自动更新

--- workspace/encrypt-tool/decrypt.js
+++ workspace/encrypt-tool/decrypt.js
@@ -1,68 +1,60 @@
-const { encrypt, decrypt } = require('./encrypt-tool/encrypt.js');
+// 解密工具
 
-// 解密函数
-function decrypt(encryptedText, method, key) {
-  switch (method) {
-    case 'caesar':
-      return caesarCipher(encryptedText, -key);
-    case 'base64':
-      return base64Decode(encryptedText);
-    case 'rot13':
-      return rot13(encryptedText, -13);
-    case 'xor':
-      return xor(encryptedText, key);
-    case 'aes':
-      return aesDecrypt(encryptedText, key);
-    default:
-      throw new Error('Unsupported decryption method');
+const crypto = require('crypto');
+const fs = require('fs');
+
+const methods = {
+  caesar: (text, key) => {
+    return text.split('').map(char => {
+      if (char.match(/[a-z]/i)) {
+        let code = char.charCodeAt(0);
+        code = code >= 65 && code <= 90 ? code - key : code - key + 32;
+        code = code > 90 ? code + 26 : code < 65 ? code - 26 : code;
+        return String.fromCharCode(code);
+      }
+      return char;
+    }).join('');
+  },
+
+  base64: text => {
+    return Buffer.from(text, 'base64').toString('utf8');
+  },
+
+  rot13: text => {
+    return text.split('').map(char => {
+      if (char.match(/[a-z]/i)) {
+        let code = char.charCodeAt(0);
+        code = code >= 97 && code <= 122 ? code - 13 : code + 13;
+        code = code > 122 ? code - 26 : code < 97 ? code + 26 : code;
+        return String.fromCharCode(code);
+      }
+      return char;
+    }).join('');
+  },
+
+  xor: text => {
+    let key = Buffer.alloc(text.length, 0x5a);
+    return Buffer.from(text, 'hex').concat(key).toString('utf8');
+  },
+
+  aes: (text, key) => {
+    const decipher = crypto.createDecipher('aes-256-cbc', key);
+    let decrypted = decipher.update(text, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
   }
-}
+};
 
-// 凯撒密码解密
-function caesarCipher(text, key) {
-  return text.split('').map(char => {
-    if (char.match(/[a-z]/i)) {
-      let code = char.charCodeAt(0) - key;
-      if (code < 'a'.charCodeAt(0)) code += 26;
-      if (code > 'z'.charCodeAt(0)) code -= 26;
-      return String.fromCharCode(code);
-    }
-    return char;
-  }).join('');
-}
-
-// Base64解码
-function base64Decode(text) {
-  return Buffer.from(text, 'base64').toString();
-}
-
-// ROT13解密
-function rot13(text) {
-  return text.split('').map(char => {
-    if (char.match(/[a-z]/i)) {
-      let code = char.charCodeAt(0) - 13;
-      if (code < 'a'.charCodeAt(0)) code += 26;
-      if (code > 'z'.charCodeAt(0)) code -= 26;
-      return String.fromCharCode(code);
-    }
-    return char;
-  }).join('');
-}
-
-// XOR解密
-function xor(encryptedText, key) {
-  return encryptedText.split('').map((char, index) => {
-    const keyChar = key[index % key.length];
-    return String.fromCharCode(char.charCodeAt(0) ^ keyChar.charCodeAt(0));
-  }).join('');
-}
-
-// AES解密
-function aesDecrypt(encryptedText, key) {
-  const decipher = crypto.createDecipher('aes-256-cbc', key);
-  let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-  return decrypted;
-}
+const decrypt = (text, method, key, output) => {
+  if (!methods[method]) {
+    throw new Error('Unsupported method');
+  }
+  const decryptedText = methods[method](text, key);
+  if (output) {
+    fs.writeFileSync(output, decryptedText);
+  } else {
+    console.log(decryptedText);
+  }
+};
 
 module.exports = { decrypt };
