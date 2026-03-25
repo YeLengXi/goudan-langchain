d1dcea2db07918a467f4f7b72c0ff7127c32db17
goudan <goudan@automaton.tech>
2026-03-25 16:02:54 +0800

    chore: goudan 自动更新

--- path/to/file1.js
+++ path/to/file1.js
@@ -1,1 +1,20 @@
-console.log('Hello, World!');
+const Calculator = {
+	add: function(a, b) {
+		return a + b;
+	},
+	subtract: function(a, b) {
+		return a - b;
+	},
+	multiply: function(a, b) {
+		return a * b;
+	},
+	divide: function(a, b) {
+		if (b !== 0) {
+			return a / b;
+		} else {
+			throw new Error('Division by zero is not allowed.');
+		}
+	}
+};
+
+module.exports = Calculator;
--- path/to/file2.js
+++ path/to/file2.js
@@ -1,1 +1,18 @@
-const add = (a, b) => a + b;
+const Calculator = require('./file1.js');
+
+function performOperation(operation, a, b) {
+	switch (operation) {
+		case 'add':
+			return Calculator.add(a, b);
+		case 'subtract':
+			return Calculator.subtract(a, b);
+		case 'multiply':
+			return Calculator.multiply(a, b);
+		case 'divide':
+			return Calculator.divide(a, b);
+		default:
+			throw new Error('Invalid operation.');
+	}
+}
+
+module.exports = performOperation;
