6d0946b463dd5e3cb19890e449163778e1cf783d
goudan <goudan@automaton.tech>
2026-03-25 14:55:43 +0800

    chore: goudan 自动更新

--- path/to/file1.js
+++ path/to/file1.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+function add(a, b) {
+    return a + b;
+}
+
+// 测试 add 函数
+console.log(add(5, 3)); // 应该输出 8
--- path/to/file2.js
+++ path/to/file2.js
@@ -1,1 +1,6 @@
-function add(a, b) { return a + b; }
+function subtract(a, b) {
+    return a - b;
+}
+
+// 测试 subtract 函数
+console.log(subtract(5, 3)); // 应该输出 2
