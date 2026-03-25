99ce243ab8e9e64d43078624a0349505aa6b1abe
goudan <goudan@automaton.tech>
2026-03-25 14:13:12 +0800

    chore: goudan 自动更新

--- workspace/code-formatter-v3.cjs
+++ workspace/code-formatter-v3.cjs
@@ -1,5 +1,9 @@
+#!/usr/bin/env node
+
+
 const fs = require('fs');
 
+// Default configuration
 const DEFAULT_CONFIG = {
   indentSize: 2,
   maxLineLength: 80,
@@ -34,7 +38,8 @@ function formatCode(code, config = DEFAULT_CONFIG) {
   formatted = formatted.replace(/(function|if|for|while|else)/g, '\n$1');
 
   // Clean up multiple newlines
-  formatted = formatted.replace(/\n{3,}/g, '\n\n');
+  formatted = formatted.replace(/
+{3,}/g, '\n\n');
 
   // Process indentation
   const lines = formatted.split('\n');
@@ -72,3 +77,35 @@ function formatCode(code, config = DEFAULT_CONFIG) {
 
 function parseArgs(args) {
   const config = { ...DEFAULT_CONFIG };
+  const files = [];
+
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    if (arg === '-i' || arg === '--indent') {
+      config.indentSize = parseInt(args[++i]) || 2;
+    } else if (arg === '-o' || arg === '--output') {
+      config.outputFile = args[++i];
+    } else if (arg === '--no-arrow') {
+      config.formatArrowFunctions = false;
+    } else if (arg === '--no-objects') {
+      config.formatObjects = false;
+    } else if (arg === '--no-arrays') {
+      config.formatArrays = false;
+    } else if (arg === '-I' || arg === '--inplace') {
+      config.inplace = true;
+    } else if (arg === '-h' || arg === '--help') {
+      printHelp();
+      process.exit(0);
+    } else if (!arg.startsWith('-')) {
+      files.push(arg);
+    }
+  }
+
+  return { config, files };
+}
+
+function printHelp() {
+  console.log('JavaScript Code Formatter v2.1');
+  console.log('Built by 狗蛋儿 (goudan) - AI Developer');
+  console.log('\nUsage: node code-formatter-v3.cjs [options] <file>');
+  console.log('\nOptions:\n  -i, --indent <spaces>   Set indent size (default: 2)\n  -o, --output <file>     Output to file instead of stdout\n  -I, --inplace           Modify file in place\n  --no-arrow             Skip arrow function formatting\n  --no-objects           Skip object formatting\n  --no-arrays            Skip array formatting\n  -h, --help             Show this help\n\nExamples:\n  node code-formatter-v3.cjs input.js\n  node code-formatter-v3.cjs -i 4 -o output.js input.js\n  node code-formatter-v3.cjs -I input.js  (modify in place)\n'}
